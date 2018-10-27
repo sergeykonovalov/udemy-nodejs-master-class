cde8cb2cc71e5fefea30e02c5264d6becfff7e03
Sergey Konovalov <skonovalov@gmail.com>
2018-10-28 00:55:18 +0300

    Update example of http server with callback functions and request/response

--- snippets/ch02/http.js
+++ snippets/ch02/http.js
@@ -1,8 +1,18 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer(function(req, res) {
+  if (req.url === '/') {
+    res.write('Root');
+    res.end();
+  }
 
-server.on('connection', (socket) => {
-  console.log(socket);
+  if(req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
+
+// server.on('connection', (socket) => {
+//   console.log(socket);
+// });
 
 server.listen(3000);
