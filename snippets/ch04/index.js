081113945e3cde975a8cab0371fd2fa7e7ab50cd
Sergey Konovalov <skonovalov@gmail.com>
2018-10-28 23:15:55 +0300

    Add sample PUT handler

--- snippets/ch04/index.js
+++ snippets/ch04/index.js
@@ -44,4 +44,25 @@ app.post('/api/courses/', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+  // Try to find course, or return 404
+  const course = courses.find(course => course.id === parseInt(req.params.id));
+  if (!course) res.status(404).send(`The course with ID ${req.params.id} is not found.`);
+
+  // Validate submitted course update, or return 400
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  // Update course and return it
+  course.name = req.body.name;
+  res.send(course);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
