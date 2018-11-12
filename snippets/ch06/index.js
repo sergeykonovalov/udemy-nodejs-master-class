d79ee6a6ed1ef29874a00cb2c4f90ce5716074e5
Sergey Konovalov <skonovalov@gmail.com>
2018-11-13 00:59:59 +0300

    Add more callbacks

--- snippets/ch06/index.js
+++ snippets/ch06/index.js
@@ -1,5 +1,23 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Inside of timed out...');
-}, 2000);
+const user = getUser(1, function(id) {
+    console.log(`Received ID: ${id}`);
+    getRepositories(id, function(repositories) {
+        console.log(repositories);
+    });
+});
+console.log(user); // undefined
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log(`Inside of timed out... { id: ${id} }`);
+        callback({ id });
+    }, 2000);
+}
+
+function getRepositories(id, callback) {
+    setTimeout(() => {
+        console.log('Inside of getRepositories()');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 3000);
+}
